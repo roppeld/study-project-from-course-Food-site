86a56ac5361c64ab94b12f561b655b430ad69fcb
Margarita <roppeldmargo@gmail.com>
2021-02-12 19:18:07 +0200

    12.02.2021 episodgit add -A

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use script";
+
+window.addEventListener('DOMContentLoaded', () => {
+      
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+          item.style.display = 'none';
+        });
+
+        tabs.forEach(tab => {
+           tab.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(0);
+});
