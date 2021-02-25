6d9dc25ff9c032da1ffe0c79b4da93b29aeabf15
Margarita <roppeldmargo@gmail.com>
2021-02-25 16:44:29 +0200

    class in project

--- js/script.js
+++ js/script.js
@@ -3,38 +3,38 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     //Tabs
-      
+
     const tabs = document.querySelectorAll('.tabheader__item'), //список названий меню
-          tabsContent = document.querySelectorAll('.tabcontent'), //картинка опредленного меню и его описание
-          tabsParent = document.querySelector('.tabheader__items'); //родитель, в котором находится список названий меню
+        tabsContent = document.querySelectorAll('.tabcontent'), //картинка опредленного меню и его описание
+        tabsParent = document.querySelector('.tabheader__items'); //родитель, в котором находится список названий меню
 
     function hideTabContent() { //функция скрытия картинок меню с их описанием, проходимся по псевдомассиву
         tabsContent.forEach(item => { //циклом forEach и на каждом элементе ставим запрет на демонстрацию
-          item.style.display = 'none'; //обращаемся к свойствам атрибута, меняем значение
+            item.style.display = 'none'; //обращаемся к свойствам атрибута, меняем значение
         });
 
         tabs.forEach(tab => { //проходим по списку названий меню
-           tab.classList.remove('tabheader__item_active');//убираем класс, если он там есть
+            tab.classList.remove('tabheader__item_active'); //убираем класс, если он там есть
         });
     }
 
     function showTabContent(i = 0) { //функция показа определенного контента, по умолчанию это самый первый
         tabsContent[i].style.display = 'block'; //обращаясь к конкретному элементу в псевдомассиве меняем свойство стиля
-        tabs[i].classList.add('tabheader__item_active');//и присваиваем класс, показывающий, что именно этот элемент сейчас активен
+        tabs[i].classList.add('tabheader__item_active'); //и присваиваем класс, показывающий, что именно этот элемент сейчас активен
     }
 
     hideTabContent();
     showTabContent();
 
-    tabsParent.addEventListener('click', (event) => {//присваивание события после клика на название меню, путем делегирования этих функций от родителя
-        const target = event.target;//переменная для облегчения пользования
-//event.target - элемент события, который совершенно точно был нами кликнут
-        if (target && target.classList.contains('tabheader__item')) {//проверка на объект клика и наличия у этого объекта нужного класса
-            tabs.forEach((item, i) => {//по циклу, обращаясь ещё разок к списку меню, порождая два аргумента
-               if (target == item) {//сравниваем кликнутый объект с тем, что есть в псевдомассиве, при совпадении
-                hideTabContent();//обнуляем свойства в контенте на демонстрацию и убираем всё лишнее и ненужное 
-                showTabContent(i);//указываем вторым аргументом, исходя из того, какая это итерация в цикле, какой именно контент            
-               }//надо показать пользователю
+    tabsParent.addEventListener('click', (event) => { //присваивание события после клика на название меню, путем делегирования этих функций от родителя
+        const target = event.target; //переменная для облегчения пользования
+        //event.target - элемент события, который совершенно точно был нами кликнут
+        if (target && target.classList.contains('tabheader__item')) { //проверка на объект клика и наличия у этого объекта нужного класса
+            tabs.forEach((item, i) => { //по циклу, обращаясь ещё разок к списку меню, порождая два аргумента
+                if (target == item) { //сравниваем кликнутый объект с тем, что есть в псевдомассиве, при совпадении
+                    hideTabContent(); //обнуляем свойства в контенте на демонстрацию и убираем всё лишнее и ненужное 
+                    showTabContent(i); //указываем вторым аргументом, исходя из того, какая это итерация в цикле, какой именно контент            
+                } //надо показать пользователю
             });
         }
     });
@@ -44,16 +44,16 @@ window.addEventListener('DOMContentLoaded', () => {
     //создаем дедлайн, до которого должен работать счётчик
     const deadline = '2021-03-29';
 
-    function getTimeRemaining(endTime) {//функция, вычисляющая остаток до дедлайна и расписывающая по переменными значения
+    function getTimeRemaining(endTime) { //функция, вычисляющая остаток до дедлайна и расписывающая по переменными значения
         //свычисляем разницу между дедлайном и текущим временем
         const offer = Date.parse(endTime) - Date.parse(new Date()),
-         //выводим количество дней, путем нахождения миллисекунд в дне, а потом разделения имеющихся милисекунд на это число     
-              days = Math.floor(offer / (1000 * 60 * 60 * 24)), //floor - округляет число вниз, т.е. просто откидывается хвост, без увеличения целого
-              hours = Math.floor((offer / (1000 * 60 * 60) % 24)),//берем общее кол-во милисекунд и делим на кол-во милисек в часе и не даем выйти 
-              //за пределы 24 часов 
-              minutes = Math.floor((offer / 1000 / 60) % 60),//идентично
-              seconds = Math.floor((offer/ 1000) % 60);
-              
+            //выводим количество дней, путем нахождения миллисекунд в дне, а потом разделения имеющихся милисекунд на это число     
+            days = Math.floor(offer / (1000 * 60 * 60 * 24)), //floor - округляет число вниз, т.е. просто откидывается хвост, без увеличения целого
+            hours = Math.floor((offer / (1000 * 60 * 60) % 24)), //берем общее кол-во милисекунд и делим на кол-во милисек в часе и не даем выйти 
+            //за пределы 24 часов 
+            minutes = Math.floor((offer / 1000 / 60) % 60), //идентично
+            seconds = Math.floor((offer / 1000) % 60);
+
         return { //чтобы вернуть созданные внутри функции переменные, возвращаем из функции объект
             'total': offer, //с созданными внутри него функциями с помещенными туда значениями - нашими переменными
             'days': days,
@@ -63,7 +63,7 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-    function getZero(num) {//функция помощник, добавляющая 0 перед каждой единичной цифрой
+    function getZero(num) { //функция помощник, добавляющая 0 перед каждой единичной цифрой
         if (num >= 0 && num < 10) {
             return `0${num}`;
         } else {
@@ -71,26 +71,26 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function setClock(selector, endTime) {//функция установки таймера на сайт
+    function setClock(selector, endTime) { //функция установки таймера на сайт
         const timer = document.querySelector(selector),
-              days = timer.querySelector('#days'),
-              hours = timer.querySelector('#hours'),
-              minutes = timer.querySelector('#minutes'),
-              seconds = timer.querySelector('#seconds'),
-              timeInterval = setInterval(updateClock, 1000);
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
 
-              updateClock();//сначала апдейтнуть, потом уже устанавливать
+        updateClock(); //сначала апдейтнуть, потом уже устанавливать
 
-        function updateClock() {//когда функция запустится она расчитает нужное нам время и на основании этих сведений
-            const t = getTimeRemaining(endTime);//занесенных в объект, она распихает это по нужным местам на странице
+        function updateClock() { //когда функция запустится она расчитает нужное нам время и на основании этих сведений
+            const t = getTimeRemaining(endTime); //занесенных в объект, она распихает это по нужным местам на странице
 
             days.innerHTML = getZero(t.days); //обращаемся к тегу HTML с указанным айдишником и ставим туда значение
-            hours.innerHTML = getZero(t.hours);//значение заносится туда, взятое из вынесенного объекта, обращаясь к его свойствам
+            hours.innerHTML = getZero(t.hours); //значение заносится туда, взятое из вынесенного объекта, обращаясь к его свойствам
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
 
-            if (t.total <= 0) {//если общее количество милисекунд подошло к нулю, исходя из сравнений с дедлайном. то остановить таймер
-                clearInterval(timeInterval);//а то пойдут минусовые значения
+            if (t.total <= 0) { //если общее количество милисекунд подошло к нулю, исходя из сравнений с дедлайном. то остановить таймер
+                clearInterval(timeInterval); //а то пойдут минусовые значения
             }
         }
     }
@@ -99,62 +99,125 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Modal
 
-    const modalTrigger = document.querySelectorAll('[data-modal]'),//кнопки с разными классами, но одним дата атрибутом
-          modal = document.querySelector('.modal'),
-          modalClose = document.querySelector('[data-close]');
-          
-    function modalOpen() {//выцепленные по дата атрибутам кнопки здесь получают свое событие
+    const modalTrigger = document.querySelectorAll('[data-modal]'), //кнопки с разными классами, но одним дата атрибутом
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
+
+    function modalOpen() { //выцепленные по дата атрибутам кнопки здесь получают свое событие
         modalTrigger.forEach(item => {
-           item.addEventListener('click', openModal);
+            item.addEventListener('click', openModal);
         });
     }
 
-    function openModal() {//вынесенный в отдельную функцию функционал открытия модального окна
+    function openModal() { //вынесенный в отдельную функцию функционал открытия модального окна
         modal.classList.add('show');
         modal.classList.remove('hide');
         modal.style.display = 'block';
-        document.body.style.overflow = 'hidden';//запрет на скролинг при открытом окне
-        clearInterval(modalTimer);//при первом открытии таймер обнуляется, незачем снова показывать окно
+        document.body.style.overflow = 'hidden'; //запрет на скролинг при открытом окне
+        //clearInterval(modalTimer);//при первом открытии таймер обнуляется, незачем снова показывать окно
     }
 
-    function closeModal() {//вынесеный в отдельную функцию функционал закрытия модального окна
+    function closeModal() { //вынесеный в отдельную функцию функционал закрытия модального окна
         modal.classList.remove('show');
-            modal.classList.add('hide');
-            modal.style.display = 'none';
-            document.body.style.overflow = '';//скролинг выставляется по умолчанию, уже на решение браузера
+        modal.classList.add('hide');
+        modal.style.display = 'none';
+        document.body.style.overflow = ''; //скролинг выставляется по умолчанию, уже на решение браузера
     }
 
     function modalCancel() {
-        modalClose.addEventListener('click', closeModal);//при клике на крестик - закрывается модальное окно
+        modalClose.addEventListener('click', closeModal); //при клике на крестик - закрывается модальное окно
 
-        modal.addEventListener('click', (event) => {//вешаем событие закрытия модального окна, при клике на
-           if (event.target === modal) {//любое место подложки, т.е. родителя элемента
-            closeModal();
-           }
+        modal.addEventListener('click', (event) => { //вешаем событие закрытия модального окна, при клике на
+            if (event.target === modal) { //любое место подложки, т.е. родителя элемента
+                closeModal();
+            }
         });
 
-        document.addEventListener('keydown', (event) => {//закрытие модального окна кнопкой Esc
-            if (event.code === 'Escape' && modal.classList.contains('show')) {//чтобы событие срабатывало
-                closeModal();//только если модальное окно открытое
+        document.addEventListener('keydown', (event) => { //закрытие модального окна кнопкой Esc
+            if (event.code === 'Escape' && modal.classList.contains('show')) { //чтобы событие срабатывало
+                closeModal(); //только если модальное окно открытое
             }
         });
     }
 
-    const modalTimer = setTimeout(openModal, 120000);//таймер появлеия модельного окна после 2 минут
+    // const modalTimer = setTimeout(openModal, 120000);//таймер появлеия модельного окна после 2 минут
 
-    function showModalWindowByScroll() {//pageYOffset отслеживает сколько пикселейй отлистал пользователь
+    function showModalWindowByScroll() { //pageYOffset отслеживает сколько пикселейй отлистал пользователь
         //по оси Y - горизонтальная ось. от которой идет отсчёт до самого верха
         if (window.pageYOffset + document.documentElement.clientHeight === document.documentElement
-            .scrollHeight) {//плюсуем отскроленное вверх с имеющимся сейчас на экране перед пользователем
-            openModal();//после того как сумма равна всей высоте элемента с учетом невидимого - открываем окно
-            window.removeEventListener('scroll', showModalWindowByScroll);//убираем скролл как только докрутили
+            .scrollHeight) { //плюсуем отскроленное вверх с имеющимся сейчас на экране перед пользователем
+            openModal(); //после того как сумма равна всей высоте элемента с учетом невидимого - открываем окно
+            window.removeEventListener('scroll', showModalWindowByScroll); //убираем скролл как только докрутили
             //первый раз
         }
     }
 
-    window.addEventListener('scroll', showModalWindowByScroll);//вешаем событие показа модального окна 
+    window.addEventListener('scroll', showModalWindowByScroll); //вешаем событие показа модального окна 
     //при скроле страницы
 
     modalOpen();
     modalCancel();
+
+    //Используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+             `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        7,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        20,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        '.menu .container'
+    ).render();
 });
