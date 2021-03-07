cc3010dff342accc510fa7f4de24b23656390ba5
Margarita <roppeldmargo@gmail.com>
2021-03-07 23:04:17 +0200

    slide (version 1) and begining version 2

--- js/script.js
+++ js/script.js
@@ -325,22 +325,57 @@ window.addEventListener('DOMContentLoaded', () => {
     //Sliders
 
     const slides = document.querySelectorAll('.offer__slide'),
-          prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next');
+          parentSlideContainer = document.querySelector('.offer__slider-counter'),
+          prev = parentSlideContainer.querySelector('.offer__slider-prev'),
+          next = parentSlideContainer.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
     let slideIndex = 1;
 
-    function showSlides(n) {
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
+    
 
-        if (n < slides.length) {
-            slideIndex = slides.length;
-        }
+    // showSlides(slideIndex);
 
-        slides.forEach(item => item.style.display = 'none');
-    }
+    // function totalSlideCounter() {
+    //     if (slides.length < 10) {
+    //         total.textContent = `0${slides.length}`;
+    //     } else if (slides.length > 10) {
+    //         total.textContent = slides.length;
+    //     }
+    // }
 
+    // totalSlideCounter();
+
+    // function showSlides(n) {
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     }
+
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     }
+
+    //     slides.forEach(item => item.style.display = 'none');
+    //     slides[slideIndex - 1].style.display = 'block';
+
+    //     if (slides.length < 10) {
+    //         current.textContent = `0${slideIndex}`;
+    //     } else if (slides.length > 10) {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+
+    // function moveSlides(n) {
+    //     showSlides(slideIndex += n);
+    // }
+
+    // prev.addEventListener('click', () => {
+    //     moveSlides(-1);
+    // });
+    
+    // next.addEventListener('click', () => {
+    //     moveSlides(1);
+    // });
 
     fetch('http://localhost:3000/menu')
        .then((data) => data.json())//распаковка из json формата с помощью метода json()
